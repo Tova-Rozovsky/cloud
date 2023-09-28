39b1d2ceceb1f1a2f513fa4c58e541f8ddf5942b
Tova-Rozovsky <tovaballons@gmail.com>
2023-09-28 10:12:25 +0300

    move to ubunto

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=$PATH:/path/to/google-cloud-sdk/bin
 
 handle_error() {
 
@@ -51,5 +52,39 @@ docker build -t chat-app:$version . || handle_error "Failed to build the image"
 git push origin "$version" || handle_error "Failed to push to GitHub"
 
 # Success message
+
+
+
+# Ask user if they want to push the image to Artifact Registry
+read -p "Do you want to push the image to Artifact Registry? (y/n): " push_image
+
+if [ "$push_image" == "y" ]; then
+  # Authenticate with service account impersonation
+  #gcloud auth configure-docker us-east1-docker.pkg.dev
+  #gcloud auth activate-service-account --key-file=/path/to/service-account-key.json --impersonate-service-account=artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
+
+
+
+appname="chat-app"
+   # Push the Docker image to Artifact Registry (optional)
+echo "Pushing Docker image to artifactregistry"
+gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com  
+gcloud auth configure-docker me-west1-docker.pkg.dev
+artifact_registry_image=me-west1-docker.pkg.dev/grunitech-mid-project/tovar-chat-app-images/${appname}:${Version}
+docker tag ${image_name} ${artifact_registry_image} 
+docker push ${artifact_registry_image}
+  # Push the image to Artifact Registry
+
+
+  echo "Image pushed to Artifact Registry."
+else
+  echo "Skipping image push to Artifact Registry."
+fi
+
+# Other deployment steps...
+# Add your custom deployment steps here
+
+echo "Deployment completed successfully."
+
 
 echo "Deployment successful!"
